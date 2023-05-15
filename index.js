e230e6ffce9076f4729ad8f2e4a6e0eacfe85ee1
Riya moni <shammiriya455@gmail.com>
2023-05-15 01:41:23 -0700

    add vercel file

--- index.js
+++ index.js
@@ -22,6 +22,29 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyJWT =(req,res,next)=>{
+  // console.log("abc" ,req.headers.authorization);
+
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.stutus(404).send({error:true, message:"unothorized verify"})
+  }
+
+  const token = authorization.split(" ")[1]
+ 
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded)=> {
+    if(error){
+      return res.stutus(404).send({error:true, message:"unothorized verify"})
+    }
+    req.decoded=decoded;
+    next()
+   
+  });
+ } 
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -34,7 +57,7 @@ async function run() {
 
    app.post("/jwt",(req,res)=>{
     const user = req.body;
-    var token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,  { expiresIn: '1h' });
+    var token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,  { expiresIn: '1hr' });
     res.send({token});
     console.log({token});
    })
@@ -58,7 +81,14 @@ async function run() {
      res.send(result);
   })
   
- app.get("/bookings",async(req,res)=>{
+ app.get("/bookings",verifyJWT,  async(req,res)=>{
+  
+  const decoded = req.decoded;
+  console.log( decoded)
+
+  if(decoded.email !== req.query.email){
+      return res.status(403).send({error: 1, message: 'forbidden access'})
+  }
   let quiry = {}
   if(req.query?.email){
     quiry = {email:req.query?.email}
